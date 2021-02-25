5807ef798caa642fa6c8742fc627e3907ba661eb
rabiayunusoglu <ryunusoglu@etu.edu.tr>
2021-02-25 21:22:46 +0300

    add personnel and authorized main page

---
+++ app/src/main/java/com/example/afetkurtar/Authorized_Anasayfa.java
@@ -0,0 +1,14 @@
+package com.example.afetkurtar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Authorized_Anasayfa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authorized_anasayfa);
+    }
+}
---
+++ app/src/main/java/com/example/afetkurtar/Personel_Anasayfa.java
@@ -0,0 +1,14 @@
+package com.example.afetkurtar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Personel_Anasayfa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personel_anasayfa);
+    }
+}
