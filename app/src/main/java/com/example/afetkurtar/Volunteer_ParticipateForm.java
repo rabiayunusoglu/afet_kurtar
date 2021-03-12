fbb0ee1605f20aaaea1365d784769ca1c7b14699
rabiayunusoglu <ryunusoglu@etu.edu.tr>
2021-03-12 22:49:16 +0300

    volunteer first register page done.

--- app/src/main/java/com/example/afetkurtar/Volunteer_ParticipateForm.java
+++ app/src/main/java/com/example/afetkurtar/Volunteer_ParticipateForm.java
@@ -3,23 +3,28 @@ package com.example.afetkurtar;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 
+import android.Manifest;
 import android.app.Activity;
 
 import android.content.Intent;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-
+import android.widget.Toast;
 
 
 import com.android.volley.Request;
@@ -32,6 +37,10 @@ import com.android.volley.toolbox.Volley;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
@@ -39,17 +48,17 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 
-
 public class Volunteer_ParticipateForm extends AppCompatActivity {
     RequestQueue queue;
     RadioGroup radioGroupExperience, radioGroupFirstAid;
-    private RadioButton experience,firstAid;
+    private static final int REQUEST_CODE_LOCATION_PERMISSION = 1;
+    private RadioButton experience, firstAid;
     DrawerLayout drawerLayout;
     String url = "https://afetkurtar.site/api/volunteerUser/create.php";
     private EditText ad, soyad, adres;
     private Button btn_gonder;
-    Double enlem, boylam;
-    private static boolean answerExperienced = false, answerFirstAid = false;
+    Double latitude, longtitude;
+    private static boolean answerExperienced = false, answerFirstAid = false, controlmessage = false;
     GoogleSignInClient mGoogleSignInClient;
 
     @Override
@@ -65,40 +74,52 @@ public class Volunteer_ParticipateForm extends AppCompatActivity {
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
 
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(Volunteer_ParticipateForm.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_LOCATION_PERMISSION);
+            controlmessage = true;
+        } else {
+            controlmessage = true;
+            getCurrentLocation();
+        }
+
         ad = (EditText) findViewById(R.id.participate_ad);
         soyad = (EditText) findViewById(R.id.participate_soyad);
         adres = (EditText) findViewById(R.id.participate_addres);
 
-        radioGroupExperience=findViewById(R.id.groupexperienced);
-        radioGroupFirstAid=findViewById(R.id.groupfistaid);
+        radioGroupExperience = findViewById(R.id.groupexperienced);
+        radioGroupFirstAid = findViewById(R.id.groupfistaid);
 
         btn_gonder = (Button) findViewById(R.id.participate_btn);
         btn_gonder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int radioId=radioGroupExperience.getCheckedRadioButtonId();
-                experience=findViewById(radioId);
 
-                if(experience.getText().toString().equals("EVET")){
-                    answerExperienced=true;
+
+                int radioId = radioGroupExperience.getCheckedRadioButtonId();
+                experience = findViewById(radioId);
+
+                if (experience.getText().toString().equals("EVET")) {
+                    answerExperienced = true;
+                } else {
+                    answerExperienced = false;
                 }
-                else{
-                    answerExperienced=false;
+                int radioId1 = radioGroupFirstAid.getCheckedRadioButtonId();
+                firstAid = findViewById(radioId1);
+                if (firstAid.getText().toString().equals("EVET")) {
+                    answerFirstAid = true;
+                } else {
+                    answerFirstAid = false;
                 }
-                int radioId1=radioGroupFirstAid.getCheckedRadioButtonId();
-                firstAid=findViewById(radioId1);
-                if(firstAid.getText().toString().equals("EVET")){
-                    answerFirstAid=true;
-                }
-                else{
-                    answerFirstAid=false;
-                }
+
                 JSONObject obj = new JSONObject();
                 try {
                     obj.put("volunteerName", ad.getText().toString() + " " + soyad.getText().toString());
                     obj.put("address", adres.getText().toString());
                     obj.put("isExperienced", answerExperienced);
                     obj.put("haveFirstAidCert", answerFirstAid);
+                    obj.put("latitude", latitude);
+                    obj.put("longitude", longtitude);
+
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -114,13 +135,48 @@ public class Volunteer_ParticipateForm extends AppCompatActivity {
                     }
                 });
                 queue.add(request);
+
             }
         });
 
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_LOCATION_PERMISSION && grantResults.length > 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation();
+            } else {
+                Toast.makeText(this, "permission denied!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
+    private void getCurrentLocation() {
 
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(3000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.getFusedLocationProviderClient(Volunteer_ParticipateForm.this).requestLocationUpdates(locationRequest, new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                LocationServices.getFusedLocationProviderClient(Volunteer_ParticipateForm.this).removeLocationUpdates(this);
+                if (locationResult != null && locationResult.getLocations().size() > 0) {
+                    int latestlocationIndex = locationResult.getLocations().size() - 1;
+                    latitude = locationResult.getLocations().get(latestlocationIndex).getLatitude();
+                    longtitude = locationResult.getLocations().get(latestlocationIndex).getLongitude();
+                }
+
+            }
+        }, Looper.getMainLooper());
+
+    }
 
 
     public void ClickRegisterInfo() {
@@ -188,13 +244,15 @@ public class Volunteer_ParticipateForm extends AppCompatActivity {
         signOut();
         redirectActivity(this, MainActivity.class);
     }
+
     public void ClickPersonel(View view) {
         //redirect activity to main screen
-        redirectActivity(this, Personel_Progress.class );
+        redirectActivity(this, Personel_Progress.class);
     }
+
     public void ClickAfetBolgesi(View view) {
         //redirect activity to main screen
-        redirectActivity(this, Afet_Bolgesi.class );
+        redirectActivity(this, Afet_Bolgesi.class);
     }
 
     public static void redirectActivity(Activity activity, Class aClass) {
