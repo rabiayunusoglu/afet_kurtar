010a5f1cf0ecb10d270f6f4094f5522cf1ede68b
Ömer Parlaktürk <78439540+OmrParlakturk@users.noreply.github.com>
2021-03-16 02:22:37 +0300

    #10 Added Return Button and getData()
    Added Return Button and implement basics of getting data from database. Data retrieval through database still could not be completed, but simply infrastructure was created.

--- app/src/main/java/com/example/afetkurtar/MapActivity.java
+++ app/src/main/java/com/example/afetkurtar/MapActivity.java
@@ -1,15 +1,20 @@
 package com.example.afetkurtar;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -18,6 +23,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -27,28 +38,50 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
-    private static final String TAG = "MapActivity";
 
+    private static final String TAG = "MapActivity";
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
 
+    RequestQueue queue;
     //variables
     private Boolean mLocationPermissionsGranted = false;
     private GoogleMap mMap;
     private Geocoder geocoder;
+
+    //markers
+    private ArrayList<Marker>markers;
+
+    //arrays from database data
+    ArrayList<String> markerInfoArray;
+    ArrayList<String> markerTitleArray;
+    ArrayList<String> markerLatitudeArray;
+    ArrayList<String> markerLongitudeArray;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        //init markers array on create
+        markers = new ArrayList<Marker>();
+
         getLocationPermission();
         geocoder = new Geocoder(MapActivity.this);
+        /*
+        queue = Volley.newRequestQueue(this);
+        getMarkerData(""); // burasi degisecek
+        */
     }
 
     private void initMap(){
@@ -69,7 +102,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
                     COURSE_LOCATION)== PackageManager.PERMISSION_GRANTED){
                 mLocationPermissionsGranted = true;
-                System.out.println("permissions true !???!");
+                //System.out.println("permissions true !???!");
                initMap(); // not sure !!!
             }else{
                 ActivityCompat.requestPermissions(this, permissions,LOCATION_PERMISSION_REQUEST_CODE);
@@ -154,14 +187,150 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 System.out.println("map clicked");
                 System.out.println("longitude : " + latLng.longitude);
                 System.out.println("latitude : " + latLng.latitude);
-                Marker marker = mMap.addMarker(new MarkerOptions().position(latLng).title("Another marker").draggable(true));
+                Marker marker = mMap.addMarker(new MarkerOptions().position(latLng).title("Another marker")
+                        .snippet("coordinates(" + latLng.latitude + ", " + latLng.longitude + ") \n" + "someInformation").draggable(true));
                 mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            }
+        });
+
+
+        Button btnGeri = (Button) findViewById(R.id.btn_geri);
+        btnGeri.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MapActivity.this, Afet_Bolgesi.class);
+                startActivity(intent);
             }
         });
 
     }//onMapReady
 
+    public void getMarkerData(String ID){
+        /*
+        JSONObject obj = new JSONObject();
+        try {
+            obj.put("teamID",ID);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        */
+        // ASAGISI DEGISECEK -- TEST AMACLI YAPILDI
 
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, "https://afetkurtar.site/api/users/read.php", null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            //  System.out.println(response.toString());
+                            getDisasterData(response);
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        System.out.println(error);
+                    }
+                });
+        queue.add(jsonObjectRequest);
+    }
+    public void initMarkers(){
+
+        String markerInfo ="";
+        String markerTitle="";
+        double markerLatitude=0;
+        double markerLongitude=0;
+        //markerInfo, markerTitle, markerLatitude, markerLongitude --> ourDatabase must have this info on table about disasterEvents
+
+        int ourArraySize = markerInfoArray.size();//!!! this size can be markerInfoArray.size() or markerTitleArray.size() or markerLatitudeArray.size() etc.
+
+        double focusLatLngLatitude = 0; //helper for animateCamera
+        double focusLatLngLongitude = 0; //helper for animateCamera
+
+        for(int i = 0 ; i < ourArraySize ; i++){
+            markerInfo = markerInfoArray.get(i);
+            markerTitle = markerTitleArray.get(i);
+            markerLatitude = Double.parseDouble(markerLatitudeArray.get(i)); //if String array then convert it to double value
+            markerLongitude = Double.parseDouble(markerLongitudeArray.get(i)); //if String array then convert it to double value
+
+            focusLatLngLatitude += markerLatitude;
+            focusLatLngLongitude += markerLongitude;
+
+
+            LatLng latLng = new LatLng(markerLatitude, markerLongitude);
+            Marker marker = mMap.addMarker(new MarkerOptions().position(latLng).title(markerTitle).snippet(markerInfo));
+            markers.add(marker);
+
+            // burada orta noktaya yakin nokta bulunup camera oraya focus olsa mantklı
+
+        }
+
+        //burasi pinlenen noktaların ortasına dogru kameranin pozisyonunun degismesini saglayacak
+        focusLatLngLatitude = focusLatLngLatitude/ourArraySize;
+        focusLatLngLongitude = focusLatLngLongitude/ourArraySize;
+        LatLng focusLatLng = new LatLng(focusLatLngLatitude,focusLatLngLongitude);
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(focusLatLng));
+
+    }
+
+    public void getDisasterData(JSONObject obj) {
+        String cevap = "";
+        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<String> listXCoordinates = new ArrayList<String>();
+        ArrayList<String> listYCoordinates = new ArrayList<String>();
+        ArrayList<String> listInfoDisaster = new ArrayList<String>();
+        ArrayList<String> listTitleDisaster = new ArrayList<String>();
+
+        try {
+            //    System.out.println(response.toString());
+            cevap = obj.getString("records");
+            cevap = cevap.substring(1,cevap.length()-1);
+
+            while(cevap.indexOf(",{") >-1){
+                list.add(cevap.substring(0,cevap.indexOf(",{")));
+                cevap = cevap.substring(cevap.indexOf(",{")+1);
+            }
+            list.add(cevap);
+            for(int x = list.size()-1; x>=0;x--){
+                try {
+                    JSONObject tmp = new JSONObject(list.get(x));
+
+                    listXCoordinates.add(tmp.getString("userID"));// Degisecek
+                    listYCoordinates.add(tmp.getString("userType"));// Degisecek
+                    /*
+                    listXCoordinates.add(tmp.getString("xCoordinates"));
+                    listYCoordinates.add(tmp.getString("yCoordinates"));
+                    listInfoDisaster.add(tmp.getString("Info"));
+                    listTitleDisaster.add(tmp.getString("Title"));
+                     */
+
+                }catch (Exception e){
+                    // e.printStackTrace();
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        //for init ourArray with using db data
+        markerInfoArray = listInfoDisaster;
+        markerTitleArray = listTitleDisaster;
+        markerLatitudeArray = listXCoordinates;
+        markerLongitudeArray = listYCoordinates;
+
+        /*
+        for(int i = 0 ; i<listXCoordinates.size() ; i++){
+            System.out.println("userID --> " + listXCoordinates.get(i));
+        }
+
+        for(int i = 0 ; i<listYCoordinates.size() ; i++){
+            System.out.println("userType --> " + listYCoordinates.get(i));
+        }
+        */
+    }//coordinatesMethod
 
 
 }
